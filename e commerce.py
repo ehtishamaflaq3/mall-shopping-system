6739bfe04f69c22ffc3ce05190db8b6344b568b7
ehtishamaflaq3 <ehtishamaflaq12341234.in>
2025-08-13 12:29:13 +0500

    update

--- e commerce.py
+++ e commerce.py
@@ -1,8 +1,16 @@
 #GREAT MALL SYSTEM(CLI).
+# INSTRUCTIONS:-
 #1.ADMIN THIS PERSON HANDLE ALL THE [INVENTORY(ADD/REMOVE)]&[CASHIER(ADD/REMOVE)]&[HELPER(ADD/REMOVE)]&[REGISTERATION (ONLINE USER ACCOUNTS/)]
-#2.CASHIER THIS PERSON [TOTAL THE CART]&[CREATE BILL]. 
+#2.CASHIER THIS PERSON [TOTAL THE CART]&[CREATE BILL]&[DISCOUNT IN FINAL BILL ON RATE]. 
 #3.HELPER THIS PERSON UPDATE STOCK[ADD/REMOVE].
 #4.USER [CART(ADD/REMOVE)]&[QUANTITY(INCREASE/DECREASE)].
-#5.
-#                           [PRODUCT CLASS]
-# this class store the product details.
+# -----------------------------------------------------------------------------------------------
+
+#                           [ADMIN SECTION]
+
+
+#                           [CASHIER SECTION]
+
+#                           [HELPER SECTION]
+
+#                           [USER SECTION]
