2a4512eb00ccfb35d5efa89b452fcea64adc47c6
maaz amir <maazamir232@gmail.com>
2025-08-13 22:03:10 +0500

    update

--- e commerce.py
+++ e commerce.py
@@ -1,16 +1,1 @@
-#GREAT MALL SYSTEM(CLI).
-# INSTRUCTIONS:-
-#1.ADMIN THIS PERSON HANDLE ALL THE [INVENTORY(ADD/REMOVE)]&[CASHIER(ADD/REMOVE)]&[HELPER(ADD/REMOVE)]&[REGISTERATION (ONLINE USER ACCOUNTS/)]
-#2.CASHIER THIS PERSON [TOTAL THE CART]&[CREATE BILL]&[DISCOUNT IN FINAL BILL ON RATE]. 
-#3.HELPER THIS PERSON UPDATE STOCK[ADD/REMOVE].
-#4.USER [CART(ADD/REMOVE)]&[QUANTITY(INCREASE/DECREASE)].
-# -----------------------------------------------------------------------------------------------
-
-#                           [ADMIN SECTION]
-
-
-#                           [CASHIER SECTION]
-
-#                           [HELPER SECTION]
-
-#                           [USER SECTION]
+print("HEloo darling  ")
